cf7f0e87067b3267d23c7da353402873616f5ad6
IsThatProGenji <rionugraha01@gmail.com>
2022-02-08 22:36:13 +0700

    add database

--- config/db.js
+++ config/db.js
@@ -1,9 +1,9 @@
-const mysql = require('mysql');
+const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'facefood'
+  host: "ohunm00fjsjs1uzy.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	",
+  user: "smq7c9e3081z89s8",
+  password: "augho7pg4kgh2o5g",
+  database: "fphkjkh34r1d3ssi",
 });
 module.exports = db;
