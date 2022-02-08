502692f074707a3f36c3a3878702877ad8cac01e
IsThatProGenji <rionugraha01@gmail.com>
2022-02-08 22:42:00 +0700

    aa

--- index.js
+++ index.js
@@ -3,16 +3,13 @@ const app = express();
 const cors = require("cors");
 
 app.use(cors());
-app.use(express.json({
-    type: ['application/json', 'text/plain']
-  }));
+app.use(express.json());
 
 const userRoute = require("./routes/User");
 app.use("/user", userRoute);
 const uploadRoute = require("./routes/Upload");
 app.use("/upload", uploadRoute);
 
-
-app.listen(3001, (req, res) => {
-    console.log("server running");
+app.listen(process.env.PORT, (req, res) => {
+  console.log("server running ");
 });
